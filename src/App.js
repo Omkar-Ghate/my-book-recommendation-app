c3360b4dc8de6451d3ea124039cb836de146e620
Omkar Ghate <otg0231@gmail.com>
2021-07-18 10:43:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
+import React from "react";
 import "./styles.css";
+import { useState } from "react";
+
+import bookDB from "./data";
 
 export default function App() {
+  const [selectedGenre, setGenre] = useState("JavaScript");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> Omkar's Book Recommendations </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite books. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(bookDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "#E5E7EB",
+              borderRadius: "0.5rem",
+              padding: "0.5rem  1rem",
+              border: "1px solid black",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div className="displayContainer">
+        {bookDB[selectedGenre].map((book) => (
+          <div className="bookItem">
+            <img
+              className="bookImage"
+              src={book.photoURL}
+              alt={book.bookName}
+            ></img>
+
+            <div className="bookDetails">
+              <div style={{ fontSize: "1.8rem", marginBottom: "0.5rem" }}>
+                {" "}
+                {book.bookName}{" "}
+              </div>
+              <div style={{ fontSize: "1.2rem", marginBottom: "0.2rem" }}>
+                {" "}
+                {book.author}
+              </div>
+              <div> {book.rating} </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
