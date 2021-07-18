84d90f36deebb48bf1b7acbf13ec4c32dc39847b
Omkar Ghate <otg0231@gmail.com>
2021-07-18 10:42:10 +0530

    feat: added book data

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export default {
+  PersonalGrowth: [
+    {
+      bookName: "The Magic Of Thinking BIG",
+      author: "David J. Schwartz",
+      photoURL:
+        "https://m.media-amazon.com/images/I/814n2fhKdvL._AC_UY327_FMwebp_QL65_.jpg",
+      rating: "4.5/5"
+    },
+    {
+      bookName: "The 5 AM Club",
+      author: "Robin Sharma",
+      photoURL:
+        "https://images-na.ssl-images-amazon.com/images/I/71eijJe2ntL.jpg",
+      rating: "4/5"
+    }
+  ],
+  Finance: [
+    {
+      bookName: "The Intelligent Investor",
+      author: "Benjamin Graham",
+      photoURL:
+        "https://images-na.ssl-images-amazon.com/images/I/91+t0Di07FL.jpg",
+      rating: "5/5"
+    },
+    {
+      bookName: "Think and Grow Rich",
+      author: "Napoleon Hill",
+      photoURL:
+        "https://images-na.ssl-images-amazon.com/images/I/91mJmoqx5zL.jpg",
+      rating: "5/5"
+    }
+  ],
+  Spirituality: [
+    {
+      bookName: "Freedom From The Known",
+      author: "J. Krishnamurthy",
+      photoURL:
+        "https://m.media-amazon.com/images/I/71YJQwFT3GL._AC_UY327_FMwebp_QL65_.jpg",
+      rating: "5/5"
+    },
+    {
+      bookName: "The Power of Now",
+      author: "Eckhart Tolle",
+      photoURL:
+        "https://images-na.ssl-images-amazon.com/images/I/41cbPheOxmL._SX323_BO1,204,203,200_.jpg",
+      rating: "4.5/5"
+    }
+  ],
+  JavaScript: [
+    {
+      bookName: "Eloquent JavaScript",
+      author: "Marijn Haverbeke",
+      photoURL:
+        "https://images-na.ssl-images-amazon.com/images/I/91q8Jx+j6iL.jpg",
+      rating: "4.5/5"
+    },
+    {
+      bookName: "You Don't Know JS Yet",
+      author: "Kyle Simpson",
+      photoURL:
+        "https://images-na.ssl-images-amazon.com/images/I/71mKvD89oEL.jpg",
+      rating: "4.5/5"
+    }
+  ]
+};
